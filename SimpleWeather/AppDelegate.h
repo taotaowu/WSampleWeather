5eb290cf1f03e908e350a054ff55793c056e0c92
haitaowu <whatanost@gmail.com>
2014-06-10 09:11:41 +0800

    initial commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by 吴海涛 on 14-6-4.
+//  Copyright (c) 2014年 吴海涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
